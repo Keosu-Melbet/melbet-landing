1387380266033648f6eb3353585247b2b0346701
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-22 13:59:21 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6601', // Ví dụ màu cam đậm
+      }
+    }
   },
   plugins: [],
-};
+}
+
+
