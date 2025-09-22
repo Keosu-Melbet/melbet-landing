fa2e21d50891be0919e74f7e3ddfc1a46b52761f
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-22 16:04:55 +0300

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,4 +1,3 @@
-// pages/blog.js
 import Head from 'next/head';
 
 export default function BlogPage() {
@@ -6,11 +5,11 @@ export default function BlogPage() {
     <>
       <Head>
         <title>Blog Kèo Sư</title>
-        <meta name="description" content="Tin tức, soi kèo, mẹo cược từ Kèo Sư" />
+        <meta name="description" content="Tin tức, soi kèo, nhận định Kèo Sư" />
       </Head>
-      <main className="max-w-4xl mx-auto px-4 py-10">
-        <h1 className="text-3xl font-bold mb-6">Bài viết mới nhất</h1>
-        <p>Blog đang được cập nhật. Hãy quay lại sau!</p>
+      <main className="max-w-4xl mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold mb-4">Bài viết mới nhất</h1>
+        <p>Những dự đoán được cập nhật. Hãy quay lại sau!</p>
       </main>
     </>
   );
