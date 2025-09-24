ffdb0f284477e149ead406f6b052616212c009d0
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-24 10:12:29 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/blog/:path*',
-        destination: 'https://blog.melbetsaffiliates.com/:path*',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
