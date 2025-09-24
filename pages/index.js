b9bd9fb8858aa8e96378cbf39692278beec4bdf9
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-24 10:22:27 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Melbet Landing</h1>
       <p>This is the main page deployed on Vercel.</p>
+      <Link href="/blog">
+        <button style={{ marginTop: "20px", padding: "10px 20px", background: "blue", color: "white" }}>
+          Go to Blog
+        </button>
+      </Link>
     </div>
   )
 }
